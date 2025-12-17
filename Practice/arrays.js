d5cde012a5b7890ac23deee4ca61092675c81b8b
Kritik Patidar <kritikpatidar09@gmail.com>
2025-12-17 15:09:03 +0000

    Created extendable calculator objects

--- Practice/arrays.js
+++ Practice/arrays.js
@@ -9,13 +9,51 @@ function filterRangeInPlace(arr, a, b){
    for(let i=0;i<arr.length;i++) {
     if(arr[i] < a || arr[i] > b) {
       arr.splice(i, 1);
-      console.log(arr);
+      // console.log(arr);
       i--;
     }
    };
 }
 filterRangeInPlace(arr, 1, 4);
-console.log(arr);
+// console.log(arr);
 
-let sa = arr.slice().sort();
-console.log(sa);
+let sa = arr.slice().sort((a,b)=>b-a);
+// console.log(sa);
+
+let arr1 = ["HTML", "JavaScript", "CSS"];
+function copySorted(arr1){
+  return arr1.sort((a,b)=>a.toLowerCase().localeCompare(b.toLowerCase()));
+}
+let sorted = copySorted(arr1);
+console.log(sorted);
+
+function Calculator() {
+  this.methods = {
+    "-": (a,b)=>a-b,
+    "+": (a,b)=>a+b
+  };
+  this.calculate = function(str) {
+    let split = str.split(' '),
+    a = +split[0],
+    op = split[1],
+    b = +split[2];
+    
+    if(!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a,b);
+  };
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
+};
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+console.log(result);
