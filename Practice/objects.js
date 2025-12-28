3411b8e482130601a568f015aeb01039bcf16ec6
Kritik Patidar <kritikpatidar09@gmail.com>
2025-12-28 17:09:20 +0000

    Worked on user object

---
+++ Practice/objects.js
@@ -0,0 +1,39 @@
+const obj = {
+  name: "kritik",
+  age: 19,
+  greet: function(){
+    return "hello";
+  }
+}
+// console.log(obj);
+obj.name = "tanisha";
+// console.log(obj);
+obj.gender = "female";
+// obj.greet();
+const clone = Object.assign({}, obj);
+clone.gender = "male";
+// console.log("this  is clone  "+ clone.greet());
+// console.log(obj.greet()===clone.greet());
+
+const user = {
+  names: "amit",
+  age: 62,
+  email: "amit@example.com",
+  isAdmin: false,
+  skills: ["Javascript", "HTML"]
+}
+const settings = {
+  theme: "dark",
+  language: "en",
+  notification: true
+}
+user.age = 26;
+user.city = "indore";
+delete user.isAdmin;
+console.log(user.names+user.email+ user.skills);
+console.log(user["isAdmin"]);
+for(let key of Object.keys(user)){
+  console.log(key);
+  
+}
+
