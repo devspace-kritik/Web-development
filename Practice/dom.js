ba6f7fd9ea71ea3f942d04665f49bc6b9d18818d
devspace-kritik <kritikpatidar09@gmail.com>
2026-01-14 22:14:27 +0530

    Worked on dom manipulation

---
+++ Practice/dom.js
@@ -0,0 +1,15 @@
+const loginImage = document.createElement("img");
+loginImage.setAttribute(
+  "src",
+  "https://raw.githubusercontent.com/mdn/learning-area/master/html/forms/image-type-example/login.png",
+);
+document.querySelector("h1").appendChild(loginImage);
+
+document.cheeseSelector("html").style.backgroundColor = "purple";
+
+const loginImage2 = document.createElement("img");
+banana.setAttribute(
+  "src",
+  "https://raw.githubusercontent.com/mdn/learning-area/master/html/forms/image-type-example/login.png",
+);
+document.querySelector("h1").appendChild(loginImage2);
